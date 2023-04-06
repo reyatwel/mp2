5bb9cf02ddbe40edd50f002d5bafd800f2945558
Rey Atwel <rey.atwel@gmail.com>
2023-04-07 00:16:39 +0800

    Added body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,24 @@
+import Image from "next/image";
 import HomeHeader from "./components/HomeHeader";
-
+import HomeSearch from "./components/HomeSearch";
 
 export default function Home() {
   return (
     <>
+      {/* Header */}
       <HomeHeader />
+
+      {/* Body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image 
+          src="/banner.png"
+          width="300"  
+          height="100"        
+          alt="banner logo"
+        />
+
+        <HomeSearch />
+      </div>
     </>
   )
 }
