2b8b2955847dc00569ec8b0fac1cc71b993f2308
Rey Atwel <rey.atwel@gmail.com>
2023-04-08 14:54:12 +0800

    Fetch data from google search API

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,17 @@
-export default function WebSearchPage() {
+export default async function WebSearchPage({searchParams}) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.SEARCH_ENGINE_ID}&q=${searchParams.searchTerm}`
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+
+  const results = data.items;
+
   return (
-    <div>
-      WebSearchPage
-    </div>
+    <>
+      {results && results.map(result => <h1 key={result.title}>{result.title}</h1>)}
+    </>
   )
 }
