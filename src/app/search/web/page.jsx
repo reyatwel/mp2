c4048a3bd7cd57ce21894c9094c15f4bdf1f6e13
Rey Atwel <rey.atwel@gmail.com>
2023-04-09 02:53:42 +0800

    Modified search page for web and image

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -7,7 +7,7 @@ export default async function WebSearchPage({searchParams}) {
   await new Promise(resolve => setTimeout(resolve, 2000));
 
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.SEARCH_ENGINE_ID}&q=${searchParams.searchTerm}}&start=${startIndex}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.SEARCH_ENGINE_ID}&q=${searchParams.searchTerm}&start=${startIndex}`
   );
 
   if (!response.ok) {
