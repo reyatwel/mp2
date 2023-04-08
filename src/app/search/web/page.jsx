214951f19aeeae9545b8fa83190be0212463ee45
Rey Atwel <rey.atwel@gmail.com>
2023-04-08 16:45:14 +0800

    Handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,13 +1,26 @@
+import Link from "next/link";
+
 export default async function WebSearchPage({searchParams}) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.SEARCH_ENGINE_ID}&q=${searchParams.searchTerm}`
   );
 
+  if (!response.ok) {
+    throw new Error("Something went wrong!");
+  }
+
   const data = await response.json();
 
-  console.log(data);
-
   const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">Try searching for something else or go back to <Link href="/" className="text-[#993366]">homepage</Link>.</p>
+      </div>
+    )
+  }
 
   return (
     <>
